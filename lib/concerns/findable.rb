8ae3d5402ca388670ce58d3ee9e8263c0d379562
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-12 16:04:44 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,7 @@
 module Findable
 
-  def self.find_by_name(name)
+  def find_by_name(name)
     self.all.find {|obj| obj.name = name}
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
